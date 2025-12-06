aaa7cf7f1aaa5ecb085d718c99bd02f960f7ba66
SalusaSecondus <SalusaSecondus@users.noreply.github.com>
2025-12-05 20:31:40 -0800

    Add timer

--- aoc.m
+++ aoc.m
@@ -1,10 +1,13 @@
 aoc(day,part,smoke)
-	n data,fn
+	n data,fn,timeName s timeName="Day "_day
 	s fn="zlink ""day"_day_""""
 	x fn
 	if smoke d ^readlines("smoke"_day_".txt",.data)
 	else  d ^aocInput(day,.data)
 	set fn="^day"_day_"(.data)"
-	if part=2 set fn="part2"_fn
+	if part=2 set fn="part2"_fn,timeName="Day "_day_".2"
+	e  s timeName="Day "_day_".1"
+	d ^aocTimer("START",timeName)
 	d @fn
+	d ^aocTimer("QUIT",timeName)
 	q
---
+++ aocTimer.m
@@ -0,0 +1,10 @@
+aocTimer(action,name)
+	n time,seconds,disp s time=$zh,seconds=(86400*$p(time,",",1))+($p(time,",",2))+($p(time,",",3)/1000000)
+	i action="START" s aocTimerData(name)=seconds q
+	s seconds=seconds-aocTimerData(name)
+	k aocTimerData(name)
+	do
+	. i seconds'<1 s disp=$fn(seconds,",",3)_" s" q
+	. s disp=$fn(seconds*1000,",",3)_" ms"
+	w name,": ",disp,!
+	q
